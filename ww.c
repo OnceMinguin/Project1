b508e64ababaeb2abbd0ff942003a33562866f1e
OnceMinguin <theshien123@gmail.com>
2021-03-06 13:54:36 -0500

    current stuff

--- ww.c
+++ ww.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
+#include <ctype.h>
 
 int main(int argc, char **argv) {
     char fname[143];
     printf("Enter filename :");
-    gets(fname);
-    int fd = open(fname, O_RDONLY | O_CREAT);  // Open file
+    scanf("%s", fname);
+    int fd = open(fname, O_RDONLY);  // Open file
     if (fd == -1) { // File dead
         return EXIT_FAILURE;
     }
+
+    char c;
+    while (read(fname, &c, 1) != '\0') {
+        printf("%c", c);
+        if (isspace(c) == 0)
+            continue;
+    }
+
     return EXIT_SUCCESS;
 }
