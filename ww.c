f7a1a0f1a93a67127c4e963f68495a0265786ee3
OnceMinguin <theshien123@gmail.com>
2021-03-08 23:35:02 -0500

    current stuff

--- ww.c
+++ ww.c
@@ -21,6 +21,11 @@ int main(int argc, char **argv) {
     char word[30];
     int count = 0, line = 0;
     while (read(fd, &c, 1) != '\0') { // read file
+        if (count == 0 && c == '\n') {
+            line = 0;
+            printf("\n\n");
+            continue;
+        }
         word[count] = c;    // create word
         if (isspace(c) != 0) { // if space go in here
             line += count;  // total of chars in the line
@@ -31,7 +36,10 @@ int main(int argc, char **argv) {
             for (int i = 0; i < count; i++) { // print word
                 printf("%c", word[i]);
             }
-            printf("%c", c); // print space
+            if(c != '\n')
+                printf("%c", c); // print space character
+            else
+                printf(" ");
             line++; // add to chars in line to account for space
             count = 0; // reset counter for word
             continue;
@@ -39,5 +47,18 @@ int main(int argc, char **argv) {
         count++;
     }
 
+     line += count;  // this is to print the last line correctly >:D
+     if (line > maxPerLine) {
+        printf("\n");
+        line = 0;
+     }
+     for (int i = 0; i < count; i++) {
+         printf("%c", word[i]);
+     }
+
     return EXIT_SUCCESS;
 }
+
+// make sure we print last word based on parameter whether that be with the line before or on a new line after DONE
+// when combining lines, need to make sure we add a space since we are getting rid of the \n DONE
+// somehow keep track of full line breaks and retain them
